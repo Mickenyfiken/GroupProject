21bf72c5b51afa0c46d99fbe2ad55bb21108412d
PontusVVV <a22ponva@student.his.se>
2026-04-08 13:57:56 +0200

    LoginService Added if

--- SportsonBackendShell/Core/Service/LoginService.cs
+++ SportsonBackendShell/Core/Service/LoginService.cs
@@ -7,19 +7,30 @@ namespace SportsonBackendShell.Core.Service
 {
     public class LoginService : ILoginService
     {
+        //private readonly HttpClient _httpClient;
         private readonly ILoginRepo _loginRepo;
 
         public LoginService(ILoginRepo loginRepo)
         {
+            //_httpClient = factory.CreateClient();
             _loginRepo = loginRepo;
         }
 
-        public IActionResult LogIn([FromBody] LoginParameters parameters)
+        public IActionResult LogIn([FromBody] LoginParameters parameters) 
         {
-            var respons = _loginRepo.LogIn(parameters);
-            
+            var response = _loginRepo.LogIn(parameters);
 
-            return 
+            var json = await response.Content.ReadAsStringAsync();
+
+            if (response.IsSuccessStatusCode)
+            {
+                var data = JsonSerializer.Deserialize<>(json); //Fråga frontend json sträng till json objekt
+                return Ok(data.token);
+            }
+            else
+            {
+                return BadRequest(json);
+            }
         }
     }
 }
