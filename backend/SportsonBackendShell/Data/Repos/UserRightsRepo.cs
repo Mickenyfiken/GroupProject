0d1f0579cc8a5baa4ea12c67516c256b7f501ad1
Björn Petersson <bjorn.petersson@iths.se>
2026-04-15 10:07:21 +0200

    merged?

--- backend/SportsonBackendShell/Data/Repos/UserRightsRepo.cs
+++ backend/SportsonBackendShell/Data/Repos/UserRightsRepo.cs
@@ -36,6 +36,33 @@ namespace SportsonBackendShell.Data.Repos
                 };
 
                 var result = JsonSerializer.Deserialize<UserIdResponse>(json, options);
+                return result?.Id;
+            }
+        }
+
+        public async Task<string[]?> GetUserRolesFromId(string token, string userId)
+        {
+            var url = $"https://stage.api.sportson.se/users/user/{userId}";
+
+            _httpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", token.Replace("Bearer ", ""));
+
+            var response = await _httpClient.GetAsync(url);
+            var json = await response.Content.ReadAsStringAsync();
+
+            if (json == string.Empty)
+            {
+                return null;
+            }
+            else
+            {
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+
+                var result = JsonSerializer.Deserialize<UserRoleResponse>(json, options);
+                return result?.UserGroups;
             }
         }
     }
