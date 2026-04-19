6904f3342382430d3ffaeb4ba3b423d175125195
Björn Petersson <bjorn.petersson@iths.se>
2026-04-19 14:16:18 +0200

    Moved the hook to correct file and implemented it in App.tsx and MainLayout.tsx (does nothing now, for future use)

--- frontend/src/components/layouts/MainLayout.tsx
+++ frontend/src/components/layouts/MainLayout.tsx
@@ -1,11 +1,15 @@
-import type { ReactNode } from 'react'
+import type { ReactNode } from "react";
+import { useInactivityLogout } from "../../hooks/authHooks";
+
 
 const MainLayout = ({ children }: { children?: ReactNode }) => {
+  useInactivityLogout();
+
   return (
     <div className="min-h-screen min-w-screen grid grid-cols-12">
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
--- frontend/src/hooks/authHooks.ts
+++ frontend/src/hooks/authHooks.ts
@@ -1,11 +1,39 @@
-import { useQuery } from '@tanstack/react-query'
-import { fetchMe } from '../api/authApi'
+import { useQuery } from "@tanstack/react-query";
+import { fetchMe, logOut } from "../api/authApi";
+import { useEffect, useRef } from "react";
+import { useNavigate } from "react-router";
 
 export const useAuth = () => {
   return useQuery({
-    queryKey: ['me'],
+    queryKey: ["me"],
     queryFn: fetchMe,
     retry: false,
     staleTime: Infinity,
-  })
-}
+  });
+};
+
+const TIMEOUT_MS = 120 * 1000;
+
+export const useInactivityLogout = () => {
+  const timer = useRef<number>(undefined);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const resetTimer = () => {
+      clearTimeout(timer.current);
+      timer.current = setTimeout(async () => {
+        await logOut();
+        navigate("/login");
+      }, TIMEOUT_MS);
+    };
+
+    const events = ["mousemove", "keydown", "click", "scroll"];
+    events.forEach((e) => window.addEventListener(e, resetTimer));
+    resetTimer();
+
+    return () => {
+      clearTimeout(timer.current);
+      events.forEach((e) => window.removeEventListener(e, resetTimer));
+    };
+  }, [navigate]);
+};
