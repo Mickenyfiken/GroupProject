d4e9e870120e98cb57218672a39298e25824e1e5
Björn Petersson <bjorn.petersson@iths.se>
2026-04-20 14:30:19 +0200

    AccessToken refreshes if there are less then 50 seconds left till expiration

--- frontend/src/components/layouts/MainLayout.tsx
+++ frontend/src/components/layouts/MainLayout.tsx
@@ -1,9 +1,9 @@
 import type { ReactNode } from "react";
-import { useInactivityLogout } from "../../hooks/authHooks";
-
+import { useInactivityLogout, useTokenRefresh } from "../../hooks/authHooks";
 
 const MainLayout = ({ children }: { children?: ReactNode }) => {
   useInactivityLogout();
+  useTokenRefresh();
 
   return (
     <div className="min-h-screen min-w-screen grid grid-cols-12">
