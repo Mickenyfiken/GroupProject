c8b8ff0ed8f63747880331e78adb9953e511cac1
Björn Petersson <bjorn.petersson@iths.se>
2026-04-21 14:13:09 +0200

    Endpoint for GetAllManuals created

--- backend/SportsonBackendShell/Core/Interface/IManualsService.cs
+++ backend/SportsonBackendShell/Core/Interface/IManualsService.cs
@@ -5,5 +5,6 @@ namespace SportsonBackendShell.Core.Interface
     public interface IManualsService
     {
         Task<ManualsResponse?> GetManualsForOneHandbook(int id);
+        Task<List<ManualsResponse>?> GetAllManuals();
     }
 }
