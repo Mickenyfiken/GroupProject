ee2c1560312055aedce0e3198ca4e263e86b1cfc
Björn Petersson <bjorn.petersson@iths.se>
2026-04-21 11:17:45 +0200

    Added manuals endpoint and hardcoded mockdata in the repo

---
+++ backend/SportsonBackendShell/Core/Service/ManualsService.cs
@@ -0,0 +1,21 @@
+﻿using SportsonBackendShell.Core.Interface;
+using SportsonBackendShell.Data.Entities;
+using SportsonBackendShell.Data.Interfaces;
+
+namespace SportsonBackendShell.Core.Service
+{
+    public class ManualsService : IManualsService
+    {
+        private readonly IManualsRepo _manualRepo;
+
+        public ManualsService(IManualsRepo manualRepo)
+        {
+            _manualRepo = manualRepo;
+        }
+
+        public async Task<ManualsResponse?> GetManualsForOneHandbook(int id)
+        {
+            return await _manualRepo.GetManualsForOneHandbook(id);
+        }
+    }
+}
---
+++ backend/SportsonBackendShell/Data/Interfaces/IManualsRepo.cs
@@ -0,0 +1,9 @@
+﻿using SportsonBackendShell.Data.Entities;
+
+namespace SportsonBackendShell.Data.Interfaces
+{
+    public interface IManualsRepo
+    {
+        Task<ManualsResponse?> GetManualsForOneHandbook(int id);
+    }
+}
