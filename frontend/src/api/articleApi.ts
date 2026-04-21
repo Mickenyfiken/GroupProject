3819b2c82ced592882fe9477f5e80aa3949b622c
Mikael Andersson <mikael.andersson@iths.se>
2026-04-21 11:29:21 +0200

    Added fetch and only returns article

--- frontend/src/api/articleApi.ts
+++ frontend/src/api/articleApi.ts
@@ -1,30 +1,11 @@
-import articles from '../mock-data/mock-articles.json'
-
 export const getArticleById = async (id: string) => {
-  const article = articles.find((a) => a.id === id)
+  const article = await fetch(
+    `https://localhost:7257/api/GetArticleById/${id}`,
+  ).then((res) => res.json())
+
   if (!article) {
     throw new Error('Article not found')
   }
 
-  const prevAndNextArticle = getAdjacentArticles(id)
-
-  console.log({ ...prevAndNextArticle, ...article })
-  return { ...prevAndNextArticle, ...article }
-}
-
-const getAdjacentArticles = (id: string) => {
-  const sorted = [...articles].sort(
-    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
-  )
-
-  const index = sorted.findIndex((a) => a.id === id)
-
-  if (index === -1) {
-    throw new Error('Article not found')
-  }
-
-  return {
-    prevArticle: sorted[index - 1] ?? null,
-    nextArticle: sorted[index + 1] ?? null,
-  }
+  return article
 }
