efa7c21dfe6c014f01be0db2659c739d13b83fea
Mikael Andersson <mikael.andersson@iths.se>
2026-04-21 11:10:34 +0200

    added PrevNextArticle / Coverimage
    Added tags to article
    
    renamed prop to match frontend

--- backend/SportsonBackendShell/Data/Entities/Article.cs
+++ backend/SportsonBackendShell/Data/Entities/Article.cs
@@ -5,17 +5,24 @@ namespace SportsonBackendShell.Data.Entities
     public class Article
     {
         public int Id { get; set; }
+        public string? Slug {get; set; }
         public string? Title { get; set; }
         public string? Body { get; set; }
-        public bool Slider { get; set; }
-        public bool Updated { get; set; }
-        public string? Url { get; set; }
-        public string? Publisher { get; set; }
-        public DateTime Date_published {  get; set; }
-        public DateTime Date_updated { get; set; }
-        public DateTime? Date_present { get; set; }
-        //public Contact Published_by { get; set; }
-        //public List<Contact> Co_publisher { get; set; }
-        //public List<Tags> tags { get; set; }
+        public string? Author { get; set; }
+        public DateTime Date {  get; set; }
+        public CoverImage? CoverImage { get; set; }
+        public string[]? Tags { get; set; }
+        public PrevNextArticle? NextArticle { get; set; }
+        public PrevNextArticle? PrevArticle { get; set; }
+ 
     }
+    
+    public class PrevNextArticle
+    {
+        public int Id { get; set; }
+        public string? Slug { get; set; }
+        public string? Title { get; set; }
+    }
+
 }
+
