fb2ad693a0d5edd8f5848d2709da5e0b809054c2
Mikael Andersson <mikael.andersson@iths.se>
2026-04-21 11:31:11 +0200

    Added newslist render

---
+++ frontend/src/components/renderNewsList.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react'
+import type { NewsSummary } from '../types/newsSummary'
+import NewsCard from './newsCard/newsCard'
+import NewsListService from '../api/newsListService'
+import { Link } from 'react-router'
+
+const RenderNewsList = () => {
+  const [newsList, setNewsList] = useState<NewsSummary[]>([])
+
+  useEffect(() => {
+    const fetchNewsList = async () => {
+      const newsList = await NewsListService()
+      setNewsList(newsList)
+    }
+    fetchNewsList()
+  }, [])
+
+  return (
+    <div>
+      {newsList.map((article) => (
+        <Link
+          key={article.id}
+          to={`nyheter/${article.id}/${article.slug}`}
+          state={{ backgroundLocation: { pathname: '/' } }}
+        >
+          <NewsCard {...article} />
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default RenderNewsList
