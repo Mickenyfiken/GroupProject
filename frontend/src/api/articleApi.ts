694f4a48663bfe16af8798caf249903e48d39f85
Mikael Andersson <mikael.andersson@iths.se>
2026-04-22 11:59:17 +0200

    Changed fetch target

--- frontend/src/api/articleApi.ts
+++ frontend/src/api/articleApi.ts
@@ -1,7 +1,5 @@
 export const getArticleById = async (id: string) => {
-  const article = await fetch(`https://localhost:7257/api/GetArticleById/${id}`).then((res) =>
-    res.json(),
-  )
+  const article = await fetch(`/api/GetArticleById/${id}`).then((res) => res.json())
 
   if (!article) {
     throw new Error('Article not found')
--- frontend/src/api/newsListService.ts
+++ frontend/src/api/newsListService.ts
@@ -1,7 +1,7 @@
 import type { NewsSummary } from '../types/newsSummary'
 
 const NewsListService = async () => {
-  const response = await fetch(`https://localhost:7257/api/GetNewsSummaryList/10`)
+  const response = await fetch(`api/GetNewsSummaryList/10`)
 
   const newsList: NewsSummary[] = await response.json()
 
