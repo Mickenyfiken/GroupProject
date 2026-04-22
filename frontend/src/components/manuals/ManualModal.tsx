4c93831c40df6b5fb49e8a23020c1a2ce9d54a93
Björn Petersson <bjorn.petersson@iths.se>
2026-04-22 10:27:03 +0200

    Made ArticleModal generic and implemented it in Article

---
+++ frontend/src/components/manuals/ManualModal.tsx
@@ -0,0 +1,30 @@
+//Uncomment this when endpoint is accepted
+
+// import { useLocation, useNavigate, useParams } from 'react-router'
+// import ArticleContent from '../ArticleContent'
+// import { ContentModal } from '../genericContent/ContentModal'
+
+// const ManualModal = () => {
+//   const navigate = useNavigate()
+//   const location = useLocation()
+//   const { id } = useParams()
+//   const { data, isLoading, error } = getManualsById(id!)
+
+//   if (isLoading) return <div>Loading...</div>
+//   if (error) return <div>Error loading article</div>
+//   if (isLoading || error || !data) console.log(isLoading, error, data)
+
+//   if (!data) return <div>Article not found</div>
+
+//   const modalState = {
+//     backgroundLocation: location.state?.backgroundLocation,
+//   }
+
+//   return (
+//     <ContentModal onClose={() => navigate('/')}>
+//       <ArticleContent data={data} modalState={modalState} />
+//     </ContentModal>
+//   )
+// }
+
+// export default ManualModal
