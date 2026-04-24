1c7bf0d9242d87b0b598207ef216a53b1b271ca6
Pontus Vadström <Pontus.Vadstrom@iths.se>
2026-04-24 08:51:14 +0000

    Merged PR 65: SearchField Component
    A simple seachfield component
    Added a seachbar to manuals
    
    Related work items: #416

--- frontend/src/views/Manuals.tsx
+++ frontend/src/views/Manuals.tsx
@@ -1,10 +1,15 @@
+import { useState } from 'react'
+import SearchField from '../components/ui/SearchField'
 import { ManualExample } from '../components/manuals/ManualExample'
 import { ManualList } from '../components/manuals/ManualsList'
 
 const Manuals = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+
   return (
     <div>
       <h1>Manuals</h1>
+      <SearchField value={searchTerm} onChange={setSearchTerm} placeholder="Sök manualer..." />
       <ManualList />
       <ManualExample />
     </div>
