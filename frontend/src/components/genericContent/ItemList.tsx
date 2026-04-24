27f30dd022c37e97061356ac074596e8260f910b
Mikael Andersson <mikael.andersson@iths.se>
2026-04-24 10:05:11 +0200

    Refactor - Generic List

---
+++ frontend/src/components/genericContent/ItemList.tsx
@@ -0,0 +1,43 @@
+import { useState, useEffect, type ReactNode } from 'react'
+
+interface ItemProps<T> {
+  fetchService: () => Promise<T[]>
+  renderItem: (item: T) => ReactNode
+  emptyMessage?: string
+}
+
+export function ItemList<T>({
+  fetchService,
+  renderItem,
+  emptyMessage = 'No items found.',
+}: ItemProps<T>) {
+
+  const [items, setItems] = useState<T[]>([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<string | null>(null)
+
+  useEffect(() => {
+
+    const fetchItems = async () => {
+      try {
+        const data = await fetchService()
+        setItems(data)
+      } catch (err) {
+        setError('Failed to fetch items.')
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchItems()
+  }, [fetchService])
+
+  if (loading) return <div>Loading...</div>
+
+  if (error) return <div>{error}</div>
+
+  if (items.length === 0) return <div>{emptyMessage}</div>
+
+  return <div>{items.map((item) => renderItem(item))}</div>
+  
+}
--- frontend/src/components/news/NewsList.tsx
+++ frontend/src/components/news/NewsList.tsx
@@ -1,29 +1,25 @@
-import { Link, type Location } from 'react-router'
-import { useArticles } from '../../hooks/articleHooks'
-import type { NewsArticle } from '../../types/NewsType'
+import type { NewsSummary } from '../../types/NewsType'
 import NewsCard from './NewsCard'
+import NewsListService from '../../api/newsListService'
+import { Link } from 'react-router'
+import { ItemList } from '../genericContent/ItemList'
 
-const NewsList = ({ location }: { location: Location<unknown> }) => {
-  const { data: articles, isLoading, error } = useArticles({})
-
-  if (isLoading) return <div>Loading...</div>
-  if (error) return <div>Error loading articles</div>
-  if (!articles) return <div>Articles not found</div>
-
+const NewsFeed = ({ location }: { location: any }) => {
   return (
-    <div className="flex flex-col gap-6 p-4">
-      {articles.map((article: NewsArticle) => (
+    <ItemList<NewsSummary>
+      fetchService={NewsListService}
+      renderItem={(item) => (
         <Link
           className="w-fit"
-          key={article.id}
-          to={`nyheter/${article.id}/${article.slug}`}
+          key={item.id}
+          to={`nyheter/${item.id}/${item.slug}`}
           state={{ backgroundLocation: location }}
         >
-          <NewsCard {...article} />
+          <NewsCard {...item} />
         </Link>
-      ))}
-    </div>
+      )}
+    />
   )
 }
 
-export default NewsList
+export default NewsFeed
