0766b7feaf44d948e34ff6edd5f320758ee23c1e
Mikael Andersson <mikael.andersson@iths.se>
2026-04-24 12:11:24 +0200

    Add ManualsList with link

--- frontend/src/components/manuals/ManualsList.tsx
+++ frontend/src/components/manuals/ManualsList.tsx
@@ -1,22 +1,38 @@
 import type { BaseManual } from '../../types/manualType'
-import { ManualResourceType } from '../../types/manualType'
-import { useAllManuals } from '../../hooks/manualHooks'
+import { ItemList } from '../genericContent/ItemList'
+import { getAllManuals } from '../../api/manualApi'
+import { Link } from 'react-router'
 
-export function ManualList() {
-  const { data, isLoading, isError } = useAllManuals(10)
-
-  if (isLoading) return <p>Loading...</p>
-  if (isError) return <p>Something went wrong</p>
-
+export const ManualList = () => {
   return (
-    <ul>
-      {data.map((manual: BaseManual) => (
-        <li key={manual.id}>
-          {manual.title},{' '}
-          {manual.resources.filter((r) => r.type === ManualResourceType.Video).length} videos,{' '}
-          {manual.resources.filter((r) => r.type === ManualResourceType.File).length} files
-        </li>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        <ItemList<BaseManual>
+          fetchService={() => getAllManuals(10)}
+          renderItem={(manual) => (
+            <li key={manual.id}>
+              <Link to={`${manual.id}`}>{manual.title && <p>{manual.title} <span>{manual.description}</span></p>}</Link>
+            </li>
+          )}
+        ></ItemList>
+      </ul>
+    </div>
   )
+
+  // const { data, isLoading, isError } = useAllManuals(10)
+
+  // if (isLoading) return <p>Loading...</p>
+  // if (isError) return <p>Something went wrong</p>
+
+  // return (
+  //   <ul>
+  //     {data.map((manual: BaseManual) => (
+  //       <li key={manual.id}>
+  //         {manual.title},{' '}
+  //         {manual.resources.filter((r) => r.type === ManualResourceType.Video).length} videos,{' '}
+  //         {manual.resources.filter((r) => r.type === ManualResourceType.File).length} files
+  //       </li>
+  //     ))}
+  //   </ul>
+  // )
 }
