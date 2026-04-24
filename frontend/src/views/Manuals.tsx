161bf373df772dc269db10e9c5d6267228f5e02e
Björn Petersson <bjorn.petersson@iths.se>
2026-04-24 15:10:48 +0200

    Search needs reimplementing

--- frontend/src/views/Manuals.tsx
+++ frontend/src/views/Manuals.tsx
@@ -1,24 +1,24 @@
-import { useState } from 'react'
-import SearchField from '../components/ui/SearchField'
+// import { useState } from 'react'
+// import SearchField from '../components/ui/SearchField'
 import { ManualList } from '../components/manuals/ManualsList'
 import { ManualResourceType } from '../types/manualType'
 
 const Manuals = () => {
-  const [searchTerm, setSearchTerm] = useState('')
+  // const [searchTerm, setSearchTerm] = useState('')
 
-  const [filteredManuals, setFilteredManuals] = useState<number | null>(null)
+  // const [filteredManuals, setFilteredManuals] = useState<number | null>(null)
 
   return (
     <div>
       <h1>Manuals</h1>
-      <SearchField value={searchTerm} onChange={setSearchTerm} placeholder="Sök manualer..." />
+      {/* <SearchField value={searchTerm} onChange={setSearchTerm} placeholder="Sök manualer..." />
       <select
         onChange={(e) => setFilteredManuals(e.target.value === '' ? null : Number(e.target.value))}
-      >
-        <option value="">Alla typer</option>
-        <option value={ManualResourceType.File}>Filer</option>
-        <option value={ManualResourceType.Video}>Video</option>
-      </select>
+      > */}
+      <option value="">Alla typer</option>
+      <option value={ManualResourceType.File}>Filer</option>
+      <option value={ManualResourceType.Video}>Video</option>
+      {/* </select> */}
       <ManualList />
     </div>
   )
