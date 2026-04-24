555b6ae678f0c30c2c6c4acb9a245b45a2ea57ab
Pontus Vadström <Pontus.Vadstrom@iths.se>
2026-04-24 12:18:09 +0000

    Merged PR 69: Searchfilter in manuals
    Added a searchfilter in manuals so that you can search for a title and chose a filetype to sort from
    
    Related work items: #403

--- frontend/src/components/manuals/ManualsList.tsx
+++ frontend/src/components/manuals/ManualsList.tsx
@@ -2,15 +2,28 @@ import type { BaseManual } from '../../types/manualType'
 import { ManualResourceType } from '../../types/manualType'
 import { useAllManuals } from '../../hooks/manualHooks'
 
-export function ManualList() {
+type ManualListProps = {
+  search: string
+  filteredType: number | null
+}
+
+export function ManualList({ search, filteredType }: ManualListProps) {
   const { data, isLoading, isError } = useAllManuals(10)
 
   if (isLoading) return <p>Loading...</p>
   if (isError) return <p>Something went wrong</p>
 
+  const filteredData = data.filter((manual: BaseManual) => {
+    const matchesSearch = manual.title.toLowerCase().includes(search.toLowerCase())
+    const matchesType =
+      filteredType === null || manual.resources.some((r) => r.type === filteredType)
+
+    return matchesSearch && matchesType
+  })
+
   return (
     <ul>
-      {data.map((manual: BaseManual) => (
+      {filteredData.map((manual: BaseManual) => (
         <li key={manual.id}>
           {manual.title},{' '}
           {manual.resources.filter((r) => r.type === ManualResourceType.Video).length} videos,{' '}
