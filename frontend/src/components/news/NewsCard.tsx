2b445ecc9df52aaeee80f49acec9283069f2ee31
Mikael Andersson <mikael.andersson@iths.se>
2026-04-29 09:38:17 +0000

    Merged PR 108: Add styling to Nyheter
    Add styling to Nyheter
    
    Related work items: #445

--- frontend/src/components/news/NewsCard.tsx
+++ frontend/src/components/news/NewsCard.tsx
@@ -12,7 +12,7 @@ const NewsCard = ({
   createdAt,
 }: NewsSummary) => {
   return (
-    <div className="flex flex-col max-w-3xl p-4 bg-white rounded-lg shadow-md h-60 just">
+    <div className="flex flex-col max-w-3xl p-4 mt-4 ml-4 bg-white rounded-lg shadow-md h-60 relative overflow-hidden flex-shrink-0">
       <div className="flex flex-row flex-wrap items-center text-base text-black/50 font-family-FSE-Text">
         <span>
           {formatDate(new Date(createdAt))} | {author}
@@ -23,12 +23,12 @@ const NewsCard = ({
           ))}
         </div>
       </div>
-      <h2 className="mt-2 text-base font-bold text-article-title">{title}</h2>
-      <div className="flex flex-row flex-1 h-5 gap-2 mt-2 text-sm text-gray-700">
-        <p className="text-sm text-gray-700 omt-2 line-clamp-4">
-          <span
-            className="mt-8
-                  [&_p]:mb-4
+      <h2 className="mt-2 text-base font-bold text-article-title leading-tight">{title}</h2>
+      <div className="flex flex-row gap-4 mt-2 flex-1 overflow-hidden">
+        <div className="relative flex-1 h-full overflow-hidden">
+          <div
+            className="mt-2
+            [&_p]:mb-4
                   [&_p]:leading-relaxed
                   [&_ul]:list-disc
                   [&_ul]:pl-6
@@ -40,10 +40,14 @@ const NewsCard = ({
                   [&_a]:underline
                   [&_a]:font-semibold
                   [&_a:hover]:text-black
-                "
+                  text-gray-700
+                  line-clamp-5
+                  leading-relaxed
+                  "
             dangerouslySetInnerHTML={{ __html: body }}
           />
-        </p>
+          <div className="absolute bottom-0 left-0 w-full h-12 bg-gradient-to-t from-white to-transparent pointer-events-none" />
+        </div>
 
         <img
           className="object-cover rounded-md w-34 h-36 shrink-0"
--- frontend/src/views/Nyheter.tsx
+++ frontend/src/views/Nyheter.tsx
@@ -1,11 +1,18 @@
 import NewsList from '../components/news/NewsList'
 import { useLocation } from 'react-router'
+import SearchField from '../components/ui/SearchField'
+import { useState } from 'react'
 const Nyheter = () => {
   const location = useLocation()
+  const [searchTerm, setSearchTerm] = useState('')
 
   return (
     <>
       <h1 className="sr-only">Nyheter</h1>
+      <SearchField value={searchTerm} onChange={setSearchTerm} placeholder="Sök..." />
+      <select>
+        <option value="">Alla nyheter</option>
+      </select>
       <NewsList location={location} />
     </>
   )
